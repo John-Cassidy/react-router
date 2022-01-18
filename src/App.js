1e9fa13e2e1266a8e3dca20cfeed5e15d0d35863
John Cassidy <jpcassidy@outlook.com>
2022-01-18 10:01:54 -0500

    change App written as normal functions that take a props argument and return a JSX element

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import Navbar from './components/Navbar';
 import { Outlet } from 'react-router-dom';
 import React from 'react';
 
-function App() {
+const App = () => {
   return (
     <div>
       <Navbar />
       <Outlet />
     </div>
   );
-}
+};
 
 export default App;
