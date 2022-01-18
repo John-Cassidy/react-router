cbcf6e3838d5324d1a132dbae7742f87d23d6840
John Cassidy <jpcassidy@outlook.com>
2022-01-18 09:59:22 -0500

    initial react-hooks branch

--- src/App.js
+++ src/App.js
@@ -1,20 +1,9 @@
-import AuthContextProvider from './contexts/AuthContext';
-import Navbar from './components/Navbar';
-import React from 'react';
-import ThemeContextProvider from './contexts/ThemeContext';
 import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className='App'>
-      <div className='ui raised very padded text container segment'>
-        <AuthContextProvider>
-          <ThemeContextProvider>
-            <Navbar />
-            <TodoList />
-          </ThemeContextProvider>
-        </AuthContextProvider>
-      </div>
+      <TodoList />
     </div>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,32 +1,17 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
-import { ThemeContext } from '../contexts/ThemeContext';
+const TodoList = () => {
+  useState();
 
-class TodoList extends Component {
-  static contextType = ThemeContext;
-  render() {
-    const { isDarkTheme, darkTheme, lightTheme, changeTheme } = this.context;
-    const theme = isDarkTheme ? darkTheme : lightTheme;
-    return (
-      <div
-        className='ui list'
-        style={{
-          background: theme.background,
-          color: theme.text,
-          height: '140px',
-          textAlign: 'center',
-          paddingTop: '20px',
-        }}
-      >
-        <p className='item'>Plan the family trip</p>
-        <p className='item'>Go shopping for dinner</p>
-        <p className='item'>Go for a walk</p>
-        <button className='ui button primary' onClick={changeTheme}>
-          change the theme
-        </button>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <ul>
+        <li>Pay bills</li>
+        <li>Do your homework</li>
+        <li>Feed the dog</li>
+      </ul>
+    </div>
+  );
+};
 
 export default TodoList;
