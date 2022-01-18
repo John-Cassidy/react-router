9c3e6a64708a70c1aa07604eb649de755662228b
John Cassidy <jpcassidy@outlook.com>
2022-01-18 11:28:38 -0500

    create TodoListContext  and Provider using Hooks

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 
 import { ThemeContext } from '../contexts/ThemeContext';
+import { TodoListContext } from '../contexts/TodoListContext';
 
 // class TodoList extends Component {
 //   static contextType = ThemeContext;
@@ -30,6 +31,7 @@ import { ThemeContext } from '../contexts/ThemeContext';
 // }
 
 const TodoList = () => {
+  const { todos } = useContext(TodoListContext);
   const { isDarkTheme, darkTheme, lightTheme, changeTheme } =
     useContext(ThemeContext);
   const theme = isDarkTheme ? darkTheme : lightTheme;
@@ -44,9 +46,17 @@ const TodoList = () => {
         paddingTop: '20px',
       }}
     >
-      <p className='item'>Plan the family trip</p>
-      <p className='item'>Go shopping for dinner</p>
-      <p className='item'>Go for a walk</p>
+      {todos.length ? (
+        todos.map((todo) => {
+          return (
+            <p key={todo.id} className='item'>
+              {todo.text}
+            </p>
+          );
+        })
+      ) : (
+        <div>You have no todos</div>
+      )}
       <button className='ui button primary' onClick={changeTheme}>
         change the theme
       </button>
